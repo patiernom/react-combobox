770e6968427c8de94ccb673985f042cd6c554648
Marco Patierno <marco_patierno@msn.com>
2017-11-10 11:06:21 +0100

    add new

--- stories/combobox.js
+++ stories/combobox.js
@@ -1,33 +1,51 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action, decorateAction } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
 import Combobox from '../components/combobox';
+import countriesJSON from '../stub/country.json';
 
-const handleOnChange = decorateAction([
-  args => args.slice(0, 1)
-]);
+const sortingByLabel = (a, b) =>{
+  const nameA = a.label.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.label.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+};
+
+const getCountries = () => {
+  const countries = Object.keys(countriesJSON).map((country) => {
+    return {value: country, label: countriesJSON[country]}
+  }).sort(sortingByLabel);
+
+  return countries;
+};
 
 storiesOf('Combobox', module)
-  .add('Simple options', () => (
+  .add('Simple array of options', () => (
     <form style={{width: '60%'}}>
       <div className={'form-group'}>
-        <label htmlFor={'simpleCombobox'}>Country</label>
+        <label htmlFor={'simpleCombobox'}>Programming Languages</label>
         <Combobox
           id={'simpleCombobox'}
           alwaysOpen
           inputClassName={'form-control'}
           onChange={action('change value')}
-          placeholder={'Find'}
+          placeholder={'Find some'}
           inputValue={''}
           keyValueInput={false}
-          options={['one', 'two', 'react', 'javascript']}
+          options={['Java', 'PHP', 'JavaScript', 'Python', 'Python', 'Objective-C', 'Ruby', 'Perl', 'C', 'C++', 'C#']}
         />
       </div>
     </form>
   ))
-  .add('Simple options', () => (
+  .add('Array of key value Object options ( {value: "the value", label: "the label"} )', () => (
     <form style={{width: '60%'}}>
       <div className={'form-group'}>
         <label htmlFor={'simpleCombobox'}>Country</label>
@@ -39,7 +57,7 @@ storiesOf('Combobox', module)
           placeholder={'Find'}
           inputValue={''}
           keyValueInput
-          options={['one', 'two', 'react', 'javascript']}
+          options={getCountries()}
         />
       </div>
     </form>
