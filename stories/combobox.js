30b5686beee5d430e851b8d1322c99ccfee8ae6c
Marco Patierno <marco_patierno@msn.com>
2017-11-10 16:01:35 +0100

    fix

--- stories/combobox.js
+++ stories/combobox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action, decorateAction } from "@storybook/addon-actions";
+// import { action, decorateAction } from "@storybook/addon-actions";
 // import { linkTo } from "@storybook/addon-links";
 import Combobox from "../components/combobox";
 import countriesJSON from "../stub/country.json";
@@ -27,6 +27,32 @@ const getCountries = () => {
   return countries;
 };
 
+const getValueSimple = (evt) => {
+  console.log(evt.target.value);
+  document.getElementById("simpleComboboxValue").innerText = `value result: ${evt.target.value}`;
+};
+
+const getValueArray = (evt) => {
+  document.getElementById("arrayComboboxValue").innerText = `value result: ${evt.target.value}`;
+};
+
+const getValueExample = (evt) => {
+  document.getElementById("arrayComboboxValue").innerText = `value result: ${evt.target.value}`;
+};
+
+const ajaxRequest = (query, callback) => {
+  document.getElementById("exampleComboboxValue").innerText = `search query: ${query}`;
+
+
+  const response = [
+    {value: "result1", label: "result 1"},
+    {value: "result2", label: "result 2"},
+    {value: "result3", label: "result 3"}
+  ];
+
+  callback(response);
+};
+
 storiesOf("Combobox", module)
   .add("Simple array of options", () => (
     <form style={{width: "60%"}}>
@@ -36,29 +62,57 @@ storiesOf("Combobox", module)
           id={"simpleCombobox"}
           alwaysOpen
           inputClassName={"form-control"}
-          onChange={action("change value")}
+          onChange={getValueSimple}
           placeholder={"Find some"}
           inputValue={""}
           keyValueInput={false}
           options={["Java", "PHP", "JavaScript", "Python", "Python", "Objective-C", "Ruby", "Perl", "C", "C++", "C#"]}
         />
       </div>
+      <div style={{margin: "10px"}}>
+        <span id={"simpleComboboxValue"} style={{fontWeight: "bold"}} />
+      </div>
     </form>
   ))
-  .add("Array of key value Object options ( {value: 'the value', label: 'the label'} )", () => (
+  .add("Array of key value Object options", () => (
     <form style={{width: "60%"}}>
       <div className={"form-group"}>
-        <label htmlFor={"simpleCombobox"}>Country</label>
+        <label htmlFor={"arrayCombobox"}>Country</label>
         <Combobox
-          id={"simpleCombobox"}
+          id={"arrayCombobox"}
           alwaysOpen
           inputClassName={"form-control"}
-          onChange={action("change value")}
-          placeholder={"Find"}
+          onChange={getValueArray}
+          placeholder={"Find Country"}
           inputValue={""}
           keyValueInput
           options={getCountries()}
         />
+      </div>
+      <div style={{margin: "10px"}}>
+        <span id={"arrayComboboxValue"} style={{fontWeight: "bold"}} />
+      </div>
+    </form>
+  ))
+  .add("AJAX", () => (
+    <form style={{width: "60%"}}>
+      <div className={"form-group"}>
+        <label htmlFor={"exampleCombobox"}>Example</label>
+        <Combobox
+          id={"exampleCombobox"}
+          alwaysOpen
+          inputClassName={"form-control"}
+          onChange={getValueExample}
+          placeholder={"type result"}
+          inputValue={""}
+          keyValueInput
+          ajaxOption
+          ajaxRequest={ajaxRequest}
+          options={[]}
+        />
+      </div>
+      <div style={{margin: "10px"}}>
+        <span id={"exampleComboboxValue"} style={{fontWeight: "bold"}} />
       </div>
     </form>
   ));
